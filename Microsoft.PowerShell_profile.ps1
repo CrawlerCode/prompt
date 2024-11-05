a57a02e2519c1bdd998c74a5dda4a4c901dd5406
CrawlerCode <41094392+CrawlerCode@users.noreply.github.com>
2024-11-05 22:50:03 +0100

    feat: Update config and profile
    - Add list view for history
    - Replace oh my posh config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 cls
 
-# OhMyPosh
-oh-my-posh --init --shell pwsh --config "C:\Users\$env:UserName\AppData\Local\Programs\oh-my-posh\themes\custom.json" | Invoke-Expression
+# Oh My Posh
+oh-my-posh init pwsh --config "C:\Users\$env:UserName\AppData\Local\Programs\oh-my-posh\themes\custom.json" | Invoke-Expression
 
 # Git Posh
 Import-Module posh-git
@@ -14,12 +14,24 @@ if ($host.Name -eq 'ConsoleHost')
 {
     Import-Module PSReadLine
 
-    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+    Set-PSReadLineOption -EditMode Windows
+
     Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
     Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
+    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
     Set-PSReadLineOption -ShowToolTips
-    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
 
-    Set-PSReadlineOption -Colors @{InlinePrediction = 'DarkYellow' }
+    Set-PSReadLineOption -Colors @{
+        Command            = 'Magenta'
+        Number             = 'DarkGray'
+        Member             = 'DarkGray'
+        Operator           = 'DarkGray'
+        Type               = 'DarkGray'
+        Variable           = 'DarkGreen'
+        Parameter          = 'DarkGreen'
+        ContinuationPrompt = 'DarkGray'
+        Default            = 'DarkGray'
+      }
 }
